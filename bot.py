4b9ea1bd1db717d47f2afec7fe37a99e11c71c62
jchav97 <jchavez97@outlook.com>
2020-06-26 18:08:10 +0000

    added a synonm function, comments, cleaned up code

--- bot.py
+++ bot.py
@@ -10,20 +10,28 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 MERRIAM_TOKEN = os.getenv('MERRIAM_DICTIONARY_API')
+MERRIAM_THESAURUS_TOKEN = os.getenv('MERRIAM_THESAURUS_API')
 
 bot = commands.Bot(command_prefix='!')
 
 
-@bot.command(name='define', help='Defines a single word given as input. Ex: !define {input}')
+@bot.command(name='def', help='Defines a single word given as input. Ex: !define {input}')
 async def definitionOf(ctx, word):
     jsonDef = await grab_json_definition(word, "collegiate", MERRIAM_TOKEN)
     for i in jsonDef[0]:
         if(i == 'shortdef'):
             for j in jsonDef[0][i]:
                 await ctx.send(j)
-                #print(j,"\n")
-    #print(jsonDef)
 
+@bot.command(name='syn', help='Gives synonyms for a single word. Ex: !syn {input}')
+async def synonymOf(ctx, word):
+	limiter = 0
+	jsonSyn = await grab_json_definition(word,"thesaurus", MERRIAM_THESAURUS_TOKEN)
+	for i in jsonSyn[0]['meta']['syns'][0]:
+		await ctx.send(i)
+		limiter = limiter + 1
+		if (limiter == 4):
+			break
 
 async def grab_json_definition(word, ref, key):
     uri = "https://dictionaryapi.com/api/v3/references/" + ref + "/json/" + word + "?key=" + key
